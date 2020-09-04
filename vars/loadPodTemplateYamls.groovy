74adcaeb6fbd894fe4649ab92abe841f1191e33d
Joost van der Griendt <jvandergriendt@cloudbees.com>
2020-09-04 18:38:35 +0200

    Update loadPodTemplateYamls.groovy

--- vars/loadPodTemplateYamls.groovy
+++ vars/loadPodTemplateYamls.groovy
@@ -1,4 +1,7 @@
 function loadPodTemplateYamls() {
-  props = readProperties file:'catalog.properties'
+  println '==> loadPodTemplateYamls'
+  println ' => reading catalog.properties'
+  def props = readProperties file:'catalog.properties'
+  println " => found agent definition ${props.agentyaml}"
   return libraryResource(props.agentyaml)
 }
